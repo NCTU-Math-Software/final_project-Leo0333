4d11c7b861f7e183851d5277649cb9e5289ae622
Leo0333 <68211000+Leo0333@users.noreply.github.com>
2020-12-26 17:45:22 +0800

    Add files via upload

---
+++ project10/diceGame.m
@@ -0,0 +1,70 @@
+function [B,PB]=diceGame(A,PA,n)
+    valueNum=size(A,2);
+    P=zeros(1,n);
+    B=[];
+    % for B
+    for ii=1:(valueNum^n)
+         number=ii;
+        for kk=1:n
+            P(n-kk+1)=number-valueNum*floor(number/valueNum);
+            number=floor(number/valueNum);
+        end
+        
+        for kk=1:n
+            if P(kk)==0
+                P(kk)=valueNum;
+            end
+        end
+        
+        value=0;
+        for kk=1:n
+            value=value+A(P(kk));
+        end
+        
+        flag=0;
+        for kk=1:size(B,2)
+            if B(kk)==value
+                flag=flag+1;
+            end
+        end
+        
+        if flag==0
+            B=[B value];
+        end
+        B=sort(B);
+    end
+    
+    % for PB
+    PB=zeros(1,size(B,2));
+    for ii=1:(valueNum^n)
+        number=ii;
+        for kk=1:n
+            P(n-kk+1)=number-valueNum*floor(number/valueNum);
+            number=floor(number/valueNum);
+        end
+        
+        for kk=1:n
+            if P(kk)==0
+                P(kk)=valueNum;
+            end
+        end
+        
+        value=0;
+        for kk=1:n
+            value=value+A(P(kk));
+        end
+        
+        pro=1;
+        for kk=1:n
+            pro=pro*PA(P(kk));
+        end
+        
+        for kk=1:size(B,2)
+            if value==B(kk)
+                PB(kk)=PB(kk)+pro;
+            end
+        end
+    end
+end
+        
+        
---
+++ project10/ranVar.m
@@ -0,0 +1,41 @@
+function [S,outP]=ranVar(A,PA,f,B,PB)
+    % example:
+    % A=[1 2 3 4];
+    % PA=[1/4 1/4 1/4 1/4];
+    % B=[2 3 4 5];
+    % PB=[1/2 1/6 1/6 1/6];
+    if nargin==5
+        P=possiMatric(A,f,B);
+        S = sort(P);
+        outP=zeros(1,size(S,2));
+        
+        for ii=1:size(A,2)
+            for jj=1:size(B,2)
+                value=PA(ii)*PB(jj);
+                for kk=1:size(S,2)
+                    if S(kk)==f(A(ii),B(jj))
+                        outP(kk)=outP(kk)+value;
+                    end
+                end
+            end
+        end
+    end
+    
+    if nargin==3
+        P=possiMatric(A,f);
+        S = sort(P);
+        outP=zeros(1,size(S,2));
+        for ii=1:size(A,2)
+            value=PA(ii);
+             for kk=1:size(S,2)
+                    if S(kk)==f(A(ii))
+                        outP(kk)=outP(kk)+value;
+                    end
+             end
+        end
+    end
+end
+
+
+            
+        
